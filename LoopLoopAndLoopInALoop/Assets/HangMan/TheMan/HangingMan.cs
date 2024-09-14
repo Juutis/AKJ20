47d6110ab819fde13b64e8de99839ac1ee63c8db
Juutis <>
2024-09-14 13:49:32 +0300

    Hanging man thingsies

---
+++ LoopLoopAndLoopInALoop/Assets/HangMan/TheMan/HangingMan.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class HangingMan : MonoBehaviour
+{
+    private float balance;
+    private float currentHeading = 0;
+    private float lastHeading = 0;
+    private float targetHeading = 0;
+    private float headingTimer = 0;
+    private Animator anim;
+    private float failTimer = 0;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        Invoke("RandomizeHeading", 3.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var t = Time.time - headingTimer;
+        t = Mathf.Clamp01(t);
+        currentHeading = Mathf.Lerp(lastHeading, targetHeading, t);
+
+        balance += currentHeading * Time.deltaTime;
+
+        var input = Input.GetAxis("HangingManHorizontal");
+        balance += input * Time.deltaTime * 10.0f;
+
+        balance = Mathf.Clamp(balance, -1.0f, 1.0f);
+
+        var time = (balance + 1) / 2.0f;
+        anim.SetFloat("time", time);
+
+        if (Mathf.Abs(balance) < 0.99f) {
+            failTimer = Time.time;
+        }
+        if (Time.time - failTimer > 0.3f) {
+            Destroy(gameObject);
+            Debug.Log("LOSE");
+        }
+    }
+
+    public void RandomizeHeading() {
+        lastHeading = currentHeading;
+        targetHeading = Random.Range(-1.0f, 1.0f) * 3.0f;
+        headingTimer = Time.time;
+        Invoke("RandomizeHeading", 0.25f);
+    }
+}
